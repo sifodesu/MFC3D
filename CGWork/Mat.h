9c1936a5616823c53ef346aa5b1e47c124411089
Raphael Azoulay <raphalaz@campus.technion.ac.il>
2018-11-20 14:20:57 +0200

    Updated vec and mat classes

--- CGWork/Mat.h
+++ CGWork/Mat.h
@@ -1,19 +1,207 @@
 #pragma once
 #include "Vec.h"
 
-class Mat
+class mat2
 {
 public:
-	Vec m[4];
+	vec2 M[2];
 
-	Mat(float s = 1.0f);
-	Mat(const Vec& v1, const Vec& v2, const Vec& v3, const Vec& v4);
-	~Mat();
-	const Vec& operator[] (int i) const;
-	Vec& operator[] (int i);
-	Mat operator+ (const Mat& m) const;
-	Mat operator* (float s) const;
-	Mat operator* (const Mat& m) const;
-	Vec operator* (const Vec& v) const;
+	mat2(float s = 1.0f) {
+		M[0][0] = s;
+		M[1][1] = s;
+	}
+
+	mat2(const vec2& v1, const vec2& v2) {
+		M[0] = v1;
+		M[1] = v2;
+	}
+
+	mat2(const mat2& m) {
+		M[0] = m[0];
+		M[1] = m[1];
+	}
+
+	vec2& operator[] (int i) {
+		return M[i];
+	}
+
+	const vec2& operator[] (int i) const {
+		return M[i];
+	}
+
+	mat2 operator+ (const mat2& m) const {
+		return mat2(M[0] + m[0], M[1] + m[1]);
+	}
+
+	mat2 operator- (const mat2& m) const {
+		return mat2(M[0] - m[0], M[1] - m[1]);
+	}
+
+	mat2 operator* (float s) const {
+		return mat2(M[0] * s, M[1] * s);
+	}
+
+	mat2 operator* (const mat2& m) const {
+		mat2 n(0.0f);
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				for (int k = 0; k < 2; k++) {
+					n[i][k] += M[i][j] * m[j][k];
+				}
+			}
+		}
+		return n;
+	}
+
+	vec2 operator* (const vec2& v) const {
+		return vec2(
+			M[0][0] * v[0] + M[0][1] * v[1],
+			M[1][0] * v[0] + M[1][1] * v[1]
+		);
+	}
+
+	mat2 operator/ (float s) const {
+		return *this * (1.0f / s);
+	}
+};
+
+class mat3
+{
+public:
+	vec3 M[3];
+
+	mat3(float s = 1.0f) {
+		M[0][0] = s;
+		M[1][1] = s;
+		M[2][2] = s;
+	}
+
+	mat3(const vec3& v1, const vec3& v2, const vec3& v3) {
+		M[0] = v1;
+		M[1] = v2;
+		M[2] = v3;
+	}
+
+	mat3(const mat3& m) {
+		M[0] = m[0];
+		M[1] = m[1];
+		M[2] = m[2];
+	}
+
+	vec3& operator[] (int i) {
+		return M[i];
+	}
+
+	const vec3& operator[] (int i) const {
+		return M[i];
+	}
+
+	mat3 operator+ (const mat3& m) const {
+		return mat3(M[0] + m[0], M[1] + m[1], M[2] + m[2]);
+	}
+
+	mat3 operator- (const mat3& m) const {
+		return mat3(M[0] - m[0], M[1] - m[1], M[2] - m[2]);
+	}
+
+	mat3 operator* (float s) const {
+		return mat3(M[0] * s, M[1] * s, M[2] * s);
+	}
+
+	mat3 operator* (const mat3& m) const {
+		mat3 n(0.0f);
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 3; k++) {
+					n[i][k] += M[i][j] * m[j][k];
+				}
+			}
+		}
+		return n;
+	}
+
+	vec3 operator* (const vec3& v) const {
+		return vec3(
+			M[0][0] * v[0] + M[0][1] * v[1] + M[0][2] * v[2],
+			M[1][0] * v[0] + M[1][1] * v[1] + M[1][2] * v[2],
+			M[2][0] * v[0] + M[2][1] * v[1] + M[2][2] * v[2]
+		);
+	}
+
+	mat3 operator/ (float s) const {
+		return *this * (1.0f / s);
+	}
+};
+
+class mat4
+{
+public:
+	vec4 M[4];
+
+	mat4(float s = 1.0f) {
+		M[0][0] = s;
+		M[1][1] = s;
+		M[2][2] = s;
+		M[3][3] = s;
+	}
+
+	mat4(const vec4& v1, const vec4& v2, const vec4& v3, const vec4& v4) {
+		M[0] = v1;
+		M[1] = v2;
+		M[2] = v3;
+		M[3] = v3;
+	}
+
+	mat4(const mat4& m) {
+		M[0] = m[0];
+		M[1] = m[1];
+		M[2] = m[2];
+		M[3] = m[3];
+	}
+
+	vec4& operator[] (int i) {
+		return M[i];
+	}
+
+	const vec4& operator[] (int i) const {
+		return M[i];
+	}
+
+	mat4 operator+ (const mat4& m) const {
+		return mat4(M[0] + m[0], M[1] + m[1], M[2] + m[2], M[3] + m[3]);
+	}
+
+	mat4 operator- (const mat4& m) const {
+		return mat4(M[0] - m[0], M[1] - m[1], M[2] - m[2], M[3] - m[3]);
+	}
+
+	mat4 operator* (float s) const {
+		return mat4(M[0] * s, M[1] * s, M[2] * s, M[3] * s);
+	}
+
+	mat4 operator* (const mat4& m) const {
+		mat4 n(0.0f);
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 4; k++) {
+					n[i][k] += M[i][j] * m[j][k];
+				}
+			}
+		}
+		return n;
+	}
+
+	vec4 operator* (const vec4& v) const {
+		return vec4(
+			M[0][0] * v[0] + M[0][1] * v[1] + M[0][2] * v[2] + M[0][3] * v[3],
+			M[1][0] * v[0] + M[1][1] * v[1] + M[1][2] * v[2] + M[1][3] * v[3],
+			M[2][0] * v[0] + M[2][1] * v[1] + M[2][2] * v[2] + M[2][3] * v[3],
+			M[3][0] * v[0] + M[3][1] * v[1] + M[3][2] * v[2] + M[3][3] * v[3]
+		);
+	}
+
+	mat4 operator/ (float s) const {
+		return *this * (1.0f / s);
+	}
 };
 
--- CGWork/Vec.h
+++ CGWork/Vec.h
@@ -1,5 +1,103 @@
 #pragma once
-class Vec
+
+class vec2
+{
+public:
+	float x;
+	float y;
+
+	vec2(float s = 0.0f) :
+		x(s), y(s) {}
+
+	vec2(float x, float y) :
+		x(x), y(y) {}
+
+	vec2(const vec2& v) :
+		x(v.x), y(v.y) {}
+
+	float& operator[] (int i) {
+		return *(&x + i);
+	}
+
+	const float& operator[] (int i) const {
+		return *(&x + i);
+	}
+
+	vec2 operator+ (const vec2& v) const {
+		return vec2(x + v.x, y + v.y);
+	}
+
+	vec2 operator- () const {
+		return vec2(-x, -y);
+	}
+
+	vec2 operator- (const vec2& v) const {
+		return vec2(x - v.x, y - v.y);
+	}
+
+	vec2 operator* (float s) const {
+		return vec2(x * s, y * s);
+	}
+
+	vec2 operator* (const vec2& v) const {
+		return vec2(x * v.x, y * v.y);
+	}
+
+	vec2 operator/ (float s) const {
+		return *this * (1.0f / s);
+	}
+};
+
+class vec3
+{
+public:
+	float x;
+	float y;
+	float z;
+
+	vec3(float s = 0.0f) :
+		x(s), y(s), z(s) {}
+
+	vec3(float x, float y, float z) :
+		x(x), y(y), z(z) {}
+
+	vec3(const vec3& v) :
+		x(v.x), y(v.y), z(v.z) {}
+
+	float& operator[] (int i) {
+		return *(&x + i);
+	}
+
+	const float& operator[] (int i) const {
+		return *(&x + i);
+	}
+
+	vec3 operator+ (const vec3& v) const {
+		return vec3(x + v.x, y + v.y, z + v.z);
+	}
+
+	vec3 operator- () const {
+		return vec3(-x, -y, -z);
+	}
+
+	vec3 operator- (const vec3& v) const {
+		return vec3(x - v.x, y - v.y, z - v.z);
+	}
+
+	vec3 operator* (float s) const {
+		return vec3(x * s, y * s, z * s);
+	}
+
+	vec3 operator* (const vec3& v) const {
+		return vec3(x * v.x, y * v.y, z * v.z);
+	}
+
+	vec3 operator/ (float s) const {
+		return *this * (1.0f / s);
+	}
+};
+
+class vec4
 {
 public:
 	float x;
@@ -7,13 +105,44 @@ public:
 	float z;
 	float w;
 
-	Vec(float s = 0.0f);
-	Vec(float x, float y, float z, float w = 1);
-	~Vec();
-	float operator[](int i) const;
-	float& operator[](int i);
-	Vec operator+ (const Vec& v) const;
-	Vec operator* (float s) const;
-	Vec operator* (const Vec& v) const;
-};
+	vec4(float s = 0.0f) :
+		x(s), y(s), z(s), w(s) {}
 
+	vec4(float x, float y, float z, float w) :
+		x(x), y(y), z(z), w(w) {}
+
+	vec4(const vec4& v) :
+		x(v.x), y(v.y), z(v.z), w(v.w) {}
+
+	float& operator[] (int i) {
+		return *(&x + i);
+	}
+
+	const float& operator[] (int i) const {
+		return *(&x + i);
+	}
+
+	vec4 operator+ (const vec4& v) const {
+		return vec4(x + v.x, y + v.y, z + v.z, w + v.w);
+	}
+
+	vec4 operator- () const {
+		return vec4(-x, -y, -z, -w);
+	}
+
+	vec4 operator- (const vec4& v) const {
+		return vec4(x - v.x, y - v.y, z - v.z, w - v.w);
+	}
+
+	vec4 operator* (float s) const {
+		return vec4(x * s, y * s, z * s, w * s);
+	}
+
+	vec4 operator* (const vec4& v) const {
+		return vec4(x * v.x, y * v.y, z * v.z, w * v.w);
+	}
+
+	vec4 operator/ (float s) const {
+		return *this * (1.0f / s);
+	}
+};
